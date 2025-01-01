a7f8e671c2b4e26f3689994c31d8529a320eb754
AQuoc98 <badguykendf@gmail.com>
2025-01-01 21:12:32 +0700

    feat(ex3): create UI and apply MediaQuery

---
+++ imic_flutter/lib/ex3/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenWidth = MediaQuery.of(context).size.width;
+    final bool _isSmallScreen = _screenWidth < 480;
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: _isSmallScreen ? _buildSmallScreen() : _buildLargeScreen());
+  }
+
+  Widget _buildCustomList() {
+    return ListView(
+      children: const <Widget>[
+        ListTile(
+          leading: Icon(Icons.map),
+          title: Text('Map'),
+        ),
+        ListTile(
+          leading: Icon(Icons.photo_album),
+          title: Text('Album'),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text('Phone'),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSmallScreen() {
+    return _buildCustomList();
+  }
+
+  Widget _buildLargeScreen() {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: _buildCustomList()),
+        VerticalDivider(width: 1, color: Colors.grey),
+        Expanded(flex: 2, child: Text('Detail Space')),
+      ],
+    );
+  }
+}
